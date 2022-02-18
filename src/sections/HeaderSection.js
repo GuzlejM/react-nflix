73ad516a0c368dd5c80d2c127c117ee75275b0e9
guzlej <martin.guzlej@gmail.com>
2022-02-18 12:43:01 +0100

    Styled Dropdown, Added pseudo classes ::before, ::after font icons

--- src/sections/HeaderSection.js
+++ src/sections/HeaderSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderWrapper from "../components/HeaderWrapper";
 import NavBar from "../components/NavBar";
+import Select from "../components/Select";
 import Logo from "../components/Logo";
 import SigninButton from "../components/SigninButton";
 import FeatureWrapper from "../components/FeatureWrapper";
@@ -8,11 +9,16 @@ import FeatureTitle from "../components/FeatureTitle";
 
 
 function HeaderSection() {
+  
   return (
     <HeaderWrapper className="header-wrapper-home">
       <NavBar className="navbar-home">
         <Logo />
+        <div className="navbar-right">
+        <Select />
+
         <SigninButton>Sign In</SigninButton>
+        </div>
       </NavBar>
       <FeatureWrapper className="feature-wrapper-home">
         <FeatureTitle className="feature-title-home">
