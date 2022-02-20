e65ec39362d14c0c2621554bd31bd09d78e9cbfa
guzlej <martin.guzlej@gmail.com>
2022-02-20 22:09:14 +0100

    installing firebas

---
+++ firebas-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJJ5TneuEgWoOjCze2pNR0IoacdZ1zUvQ",
+  authDomain: "nehflix-guzlej.firebaseapp.com",
+  projectId: "nehflix-guzlej",
+  storageBucket: "nehflix-guzlej.appspot.com",
+  messagingSenderId: "959282018914",
+  appId: "1:959282018914:web:d9f6ad49570610ec9eaf3b",
+  measurementId: "G-RYR1MZLPN6"
+};
+
+export const app = initializeApp(firebaseConfig);
