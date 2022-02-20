d03e04400fe7833dbcd1349e05c9a4304f0a6d47
guzlej <martin.guzlej@gmail.com>
2022-02-20 11:36:09 +0100

    Accordion Options in JSON initialisation Accordion Files

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,12 +2,14 @@
 import React from "react";
 import HeaderSection from "../sections/HeaderSection";
 import FooterSection from "../sections/FooterSection";
+import FormSection from "../sections/FormSection"
 
 
 function LandingPage() {
   return (
     <>
       <HeaderSection />
+      <FormSection />
       <FooterSection />
     </>
   );
